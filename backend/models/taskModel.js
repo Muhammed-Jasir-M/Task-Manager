c858dca373f07d0d39caab31c5d4b919dad31077
Muhammed Jasir M <jasirmm307@gmail.com>
2025-08-04 10:37:13 +0000

    Add app.js and task_model.js

---
+++ backend/models/taskModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  status: {
+    type: String,
+    enum: ['To Do', 'In Progress', 'Done'],
+    default: 'To Do'
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High'],
+    default: 'Medium'
+  },
+  dueDate: {
+    type: String,
+    default: new Date().toISOString()
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', TaskSchema);
