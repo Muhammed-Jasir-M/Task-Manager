376090371263f8ce1466fe788ab667026af190f2
Muhammed Jasir M <jasirmm307@gmail.com>
2025-08-06 17:03:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,7 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
   
